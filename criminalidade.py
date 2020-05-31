5d91d3de36d5dfc89cae455f3ac7487dbd1a48fe
guiga07 <guicm007@gmail.com>
2020-05-31 14:34:57 -0300

    importando dataset e implementando checkout

--- criminalidade.py
+++ criminalidade.py
@@ -7,3 +7,15 @@ import pydeck as pdk
 df = pd.read_csv('dataset\criminalidade_sp_2.csv')
 
 #dashboard
+st.title('Criminalidade em São Paulo')
+st.markdown(
+    """
+    A criminalidade é um problema recorrente no Brasil.
+    Buscamos sempre formas de diminuir esses índices e usando técnicas de **Ciências de Dados** conseguimos
+    entender melhor o que está acontecendo e gerar insights que direcionem ações capazes de diminuir os
+    índices de criminalidade.
+    """
+)
+
+if st.checkbox('Ver tabelas com dados'):
+    st.write(df)
