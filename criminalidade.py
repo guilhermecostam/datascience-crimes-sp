0aeb4147f7ddc6bf5ac0de54b2bbb01d526e5383
guiga07 <guicm007@gmail.com>
2020-05-31 14:10:19 -0300

    add datasets e .py inicial

---
+++ criminalidade.py
@@ -0,0 +1,9 @@
+#import de bibliotecas importantes
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+
+#carregando dados
+df = pd.read_csv('dataset\criminalidade_sp_2.csv')
+
+#dashboard
